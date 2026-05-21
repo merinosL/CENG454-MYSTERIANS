afe78c450ba0936cbc9cc073d423418efd9ddd7b
usbak <usbak28sdal@gmail.com>
2026-05-21 14:06:57 +0300

    Added player health system with enemy and boss damage integration

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Health Settings")]
+    public int maxHealth = 3;
+
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        Debug.Log("Player Health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player Died");
+
+        
+        gameObject.SetActive(false);
+    }
+}
