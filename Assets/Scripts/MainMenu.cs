b6baf51a3080707d180b76ad0f285006b5c6c966
unknown <usbak28sdal@gmail.com>
2026-05-25 17:14:39 +0300

    Added level exit scene transition system

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Level_1");
+        SceneManager.LoadScene("IntroScene");
     }
 
     public void QuitGame()
